08b6e397110fdb2e03c34932af777bb5367770c9
Nikolay Rademacher <rademacher.nikolay@gmail.com>
2022-03-10 22:50:40 +0100

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 import type { AppProps } from 'next/app'
 import type { AppType } from 'next/dist/shared/lib/utils'
-import type { AppRouter } from '@/server/routers/_app'
+import type { AppRouter } from '@/server/routers/app-router'
 
 import type { Maybe } from '@trpc/server'
 import { httpBatchLink } from '@trpc/client/links/httpBatchLink'
