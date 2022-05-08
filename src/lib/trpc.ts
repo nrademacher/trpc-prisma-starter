7f244886b8deb2bc666bccb3ff60009d96924b4e
Nikolay Rademacher <rademacher.nikolay@gmail.com>
2022-05-08 15:22:53 +0200

    chore: add more query and mutation infer types

--- src/lib/trpc.ts
+++ src/lib/trpc.ts
@@ -1,10 +1,21 @@
-import { createReactQueryHooks } from '@trpc/react'
+import type { inferProcedureInput, inferProcedureOutput } from '@trpc/server'
 import type { AppRouter } from '@/server/routers/app-router'
-
-import type { inferProcedureOutput } from '@trpc/server'
-
-export const trpc = createReactQueryHooks<AppRouter>()
+import { createReactQueryHooks } from '@trpc/react'
 
 export type inferQueryOutput<TRouteKey extends keyof AppRouter['_def']['queries']> = inferProcedureOutput<
     AppRouter['_def']['queries'][TRouteKey]
 >
+
+export type inferQueryInput<TRouteKey extends keyof AppRouter['_def']['queries']> = inferProcedureInput<
+    AppRouter['_def']['queries'][TRouteKey]
+>
+
+export type inferMutationOutput<TRouteKey extends keyof AppRouter['_def']['mutations']> = inferProcedureOutput<
+    AppRouter['_def']['mutations'][TRouteKey]
+>
+
+export type inferMutationInput<TRouteKey extends keyof AppRouter['_def']['mutations']> = inferProcedureInput<
+    AppRouter['_def']['mutations'][TRouteKey]
+>
+
+export const trpc = createReactQueryHooks<AppRouter>()
