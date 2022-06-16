ba3cbec20830c18c8a948e4fe95eef4f02aa3390
Nikolay Rademacher <rademacher.nikolay@gmail.com>
2022-06-16 17:10:03 +0200

    chore: remove queryClientConfig

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -70,33 +70,6 @@ export default withTRPC<AppRouter>({
                 }),
             ],
             /**
-             * @link https://react-query.tanstack.com/reference/QueryClient
-             */
-            queryClientConfig: {
-                defaultOptions: {
-                    queries: {
-                        /**
-                         * 1s should be enough to just keep identical query waterfalls low
-                         * @example if one page components uses a query that is also used further down the tree
-                         */
-                        staleTime: 1000,
-                        /**
-                         * Retry `useQuery()` calls depending on this function
-                         */
-                        retry(failureCount, _err) {
-                            const err = _err as never as Maybe<TRPCClientError<AppRouter>>
-                            const code = err?.data?.code
-                            if (code === 'BAD_REQUEST' || code === 'FORBIDDEN' || code === 'UNAUTHORIZED') {
-                                // if input data is wrong or you're not authorized there's no point retrying a query
-                                return false
-                            }
-                            const MAX_QUERY_RETRIES = 3
-                            return failureCount < MAX_QUERY_RETRIES
-                        },
-                    },
-                },
-            },
-            /**
              * @link https://trpc.io/docs/data-transformers
              */
             transformer: superjson,
