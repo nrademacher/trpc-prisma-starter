9095020390d7c6b6fe5205c1a1e644a2007110f1
Nikolay Rademacher <rademacher.nikolay@gmail.com>
2022-06-16 16:37:06 +0200

    chore: replace jest with playwright testing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -37,19 +37,13 @@ const MyApp = (({ Component, pageProps: { session, ...pageProps } }: AppPropsWit
 }) as AppType
 
 function getBaseUrl() {
-    if (process.browser) {
+    if (typeof window !== 'undefined') {
         return ''
     }
     // reference for vercel.com
     if (process.env.VERCEL_URL) {
         return `https://${process.env.VERCEL_URL}`
     }
-
-    // // reference for render.com
-    if (process.env.RENDER_INTERNAL_HOSTNAME) {
-        return `http://${process.env.RENDER_INTERNAL_HOSTNAME}:${process.env.PORT}`
-    }
-
     // assume localhost
     return `http://localhost:${process.env.PORT ?? 3000}`
 }
--- src/server/lib/sum.test.ts
+++ src/server/lib/sum.test.ts
@@ -1,3 +1,4 @@
+import { test, expect } from '@playwright/test'
 import { sum } from './sum'
 
 test('sum', () => {
