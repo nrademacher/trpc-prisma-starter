397683de862afbfb81c4cc664f939f1e686a009b
Nikolay Rademacher <rademacher.nikolay@gmail.com>
2022-06-16 16:41:34 +0200

    chore: add schema formatting on generate

--- playwright/example.spec.ts
+++ playwright/example.spec.ts
@@ -1,8 +1,8 @@
 import { test, expect } from '@playwright/test'
 
 test('should navigate to the about page', async ({ page }) => {
-  // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
-  await page.goto('http://localhost:3000/')
-  // The new page should contain an h1 with "About Page"
-  await expect(page.locator('h1')).toContainText('Welcome to your tRPC starter!')
+    // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
+    await page.goto('http://localhost:3000/')
+    // The new page should contain an h1 with "About Page"
+    await expect(page.locator('h1')).toContainText('Welcome to your tRPC starter!')
 })
